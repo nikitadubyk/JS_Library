28ca368f1de5b90cd42003d2b59734b8d43cf9f5
Nikita <nikita_dubyk@mail.ru>
2021-08-15 17:55:40 +0300

    change actions.js to handles.js and add many method on actions.js

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,15 @@ $('.active').addClass('test', 'test1', 'test2');
 // $('.active').removeClass();
 
 $('.btn').click(function() {
-    $(this).toggleClass('active');
+    $('div').eq(2).toggleClass('active');
 });
 
 $('.btn').setAtr('data-btn', 'btn').removeAtr('data-btn');
+
+// console.log($('button').html('change'));
+
+$('div').click(function() {
+    console.log($(this).index());
+});
+
+console.log($('div').eq(3).find('.more'));
